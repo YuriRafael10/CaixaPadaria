ed47318b63bed7822835ac07083a5460ee175de6
YuriRafael10 <yurilana1001@gmail.com>
2024-10-23 12:00:27 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaPadaria/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CaixaPadaria.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CaixaPadaria
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new LoginWindow();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+
+}
